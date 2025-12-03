bac33692e3a1602954d03e46f9bac66d07c98dde
mallika14-cyber <mallika.14@gmail.com>
2025-12-03 20:02:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let cart = [];
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 function addToCart(name, price) {
     cart.push({ name, price });
+    localStorage.setItem("cart", JSON.stringify(cart));
+
     alert(name + " added to cart!");
-    console.log("Cart:", cart);
 }
